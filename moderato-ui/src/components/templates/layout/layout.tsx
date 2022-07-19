9a32c585dd5e2d81133887a6a905261fb2f9636d
Naohito-T <naohito.tanaka0523@gmail.com>
2022-07-19 18:19:17 +0900

    :bug: git 大文字・小文字問題対象

---
+++ moderato-ui/src/components/templates/layout/layout.tsx
@@ -0,0 +1,54 @@
+// import { useState } from 'react';
+import { NextComponentType, NextPageContext } from 'next';
+// import { useRouter } from 'next/router';
+
+// import { Header } from '@/components/molecules';
+import styled from 'styled-components';
+
+// type LayoutProps = Required<{
+//   readonly children: ReactElement;
+// }>;
+
+type LayoutProps = {
+  showFooter?: boolean;
+  readonly children: React.ReactNode;
+};
+
+const StyledLayout = styled.div`
+  width: 100%;
+`;
+
+const StyledMain = styled.main`
+  color: #000;
+`;
+
+export const Layout: NextComponentType<NextPageContext, null, LayoutProps> = ({
+  // showFooter = true,
+  children,
+}) => {
+  // const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // const router = useRouter();
+
+  // const onClickOpenMenu = () => {
+  //   setIsMenuOpen(true);
+  // };
+
+  // const onClickCloseMenu = () => {
+  //   setIsMenuOpen(false);
+  // };
+
+  // パスが同一であればmenuを閉じる
+  // const isTopPage = (href: string) => {
+  //   if (router.pathname === href) {
+  //     onClickCloseMenu();
+  //   }
+  // };
+
+  return (
+    <StyledLayout>
+      {/* <Header onClickMenuButton={() => onClickOpenMenu()} /> */}
+      <StyledMain>{children}</StyledMain>
+      {/* {showFooter && <Footer />} */}
+    </StyledLayout>
+  );
+};
