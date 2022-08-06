5a83244f0ccdc266e5dfd6f8b6433398d099d39f
Naohito-T <naohito.tanaka0523@gmail.com>
2022-08-06 15:11:09 +0900

    :memo: gsap esmodule 対応iou

--- moderato-ui/src/libs/animation/base/_base.gsap.ts
+++ moderato-ui/src/libs/animation/base/_base.gsap.ts
@@ -1,5 +1,5 @@
 import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 // import { ScrollToPlugin } from 'gsap/all';
 
 export abstract class BaseGSAP {
