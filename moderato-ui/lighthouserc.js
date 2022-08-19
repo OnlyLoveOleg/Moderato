1426bdd7c4f96549bf2678f2646dacf1f7db3b73
Naohito-T <naohito.tanaka0523@gmail.com>
2022-08-19 11:05:51 +0900

    :green_heart: yarn cache削除した

--- moderato-ui/lighthouserc.js
+++ moderato-ui/lighthouserc.js
@@ -1,12 +1,16 @@
 module.exports = {
   ci: {
     collect: {
-      AnumberOfRuns: 1, // Lighthouse の試行回数は1回
-      startServerCommand: 'yarn start', // プロダクションモードでローカルサーバーを起動する
-      url: ['http://localhost:3000/'], // 評価対象のURL
+      // AnumberOfRuns: 1, // Lighthouse の試行回数は1回
+      // startServerCommand: 'yarn start', // プロダクションモードでローカルサーバーを起動する
+      // url: ['http://localhost:3000/'], // 評価対象のURL
+      staticDistDir: './.next',
+      url: ['/'],
     },
     upload: {
-      target: 'temporary-public-storage',
+      target: 'lhci',
+      serverBaseUrl: 'https://secret-journey-75761.herokuapp.com/',
+      token: 'b7089bc4-7020-4497-b9d0-ef2ddba0ebfd',
     },
   },
 };
